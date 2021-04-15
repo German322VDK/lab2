71781642315b5304ba0965d13baa390bdfa75dad
German322VDK <germean322@gmail.com>
2021-04-15 14:27:04 +1000

    отправка

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -5,16 +5,16 @@ namespace lab2
 {
     class Program
     {
-        static double[,] a; // массив спинов
-        static int N = 32; // количество спиннов NxN
+        static int[,] a; // массив спинов
+        static int N = 16; // количество спиннов NxN
 
         static void Main(string[] args)
         {
             System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
             double En, M; // Энергия системы и намагниченность
             string fn1 = "1.txt", fn2 = "2.txt", fn3 = "3.txt"; // имена файлов
-            StreamWriter[] sw = new StreamWriter[80]; // массив потоков файлов
-            a = new double[N, N]; // создание массива спинов
+            StreamWriter[] sw = new StreamWriter[50]; // массив потоков файлов
+            a = new int[N, N]; // создание массива спинов
             int ir, jr; // индексы рандомного спина
             double E1, E2, d, rn, C, rd, spold, Eer = 0, Mer = 0, ES = 0, ESQ = 0, MS = 0; // переменные для подсчёта энергии намагниченности теплоёмкости и тд
             double[] E = new double[5]; // массив энергий системы
@@ -33,7 +33,8 @@ namespace lab2
             {
                 for (int j = 0; j < N; j++)
                 {
-                    a[i, j] = NextDouble(-1 * Math.PI, Math.PI);
+                    if (r.Next(2) == 0) a[i, j] = 1;
+                    else a[i, j] = -1;
                 }
             }
 
@@ -50,7 +51,7 @@ namespace lab2
            
             Console.WriteLine("Начинаем:");
             
-            for (double t = 0.05; t <= 4; t += 0.05) // цикл по температуре
+            for (double t = 0.1; t <= 5; t += 0.1) // цикл по температуре
             {
                 t = Math.Round(t, 2); // округляем (ps иногда бывало 0.1000000000000003)
                 // обнуляем
@@ -59,35 +60,51 @@ namespace lab2
                 MS = 0;
                 Eer = 0;
                 Mer = 0;
-
-                for (int i = 0; i < 5; i++) // 5 раз вызываем монтекарло и считаем среднее
+                using (sw[ch] = new StreamWriter(t + ".txt")) // безопасно открываем поток записываем в файл и его закрываем
                 {
-                    for (int mk = 0; mk < 500000; mk++) // монтекарло
+                    for (int i = 0; i < 5; i++) // 5 раз вызываем монтекарло и считаем среднее
                     {
-                        ir = r.Next(N);
-                        jr = r.Next(N);
-                        rd = NextDouble(-1 * Math.PI, Math.PI);
-
-                        spold = a[ir, jr];
-                        E1 = CreatEnS(ir, jr);
-                        a[ir, jr] = rd;
-                        E2 = CreatEnS(ir, jr);
-
-                        if (E1 <= E2) 
+                        for (int mk = 0; mk < 500000; mk++) // монтекарло
                         {
-                            rn = r.NextDouble();
+                            ir = r.Next(N);
+                            jr = r.Next(N);
+                            rd = NextDouble(-1 * Math.PI, Math.PI);
 
-                            d = Math.Exp(-1 * (E2 - E1) / t);
+                            spold = a[ir, jr];
+                            E1 = CreatEnS(ir, jr);
+                            a[ir, jr] *= -1;
+                            E2 = CreatEnS(ir, jr);
 
-                            if (rn > d) a[ir, jr] = spold;
+                            if (E1 <= E2)
+                            {
+                                rn = r.NextDouble();
+
+                                d = Math.Exp(-1 * (E2 - E1) / t);
+
+                                if (rn > d) a[ir, jr] *= -1;
+                            }
+
+                            if(mk % 500 == 0)
+                            {
+                                for (int l = 0; l < N; l++)
+                                {
+                                    for (int j = 0; j < N; j++)
+                                    {
+                                        sw[ch].Write(a[l, j] + " ");
+                                    }
+                                }
+                                sw[ch].Write("\n");
+                            }
                         }
+
+                        E[i] = EnSys();
+
+                        Esq[i] = E[i] * E[i];
+
+                        Ms[i] = Magnet();
+
+                        
                     }
-
-                    E[i] = EnSys();
-
-                    Esq[i] = E[i] * E[i];
-
-                    Ms[i] = Magnet();
                 }
 
                 for (int i = 0; i < 5; i++) // считаем среднее
@@ -102,8 +119,6 @@ namespace lab2
                 ESQ /= 5;
                 M = MS /= 5;
 
-                C = (ESQ - ES * ES) / (t * t); // теплоёмкость
-
                 for (int i = 0; i < 5; i++) // считаем ошибки
                 {
                     Eer += (E[i] - ES) * (E[i] - ES);
@@ -115,66 +130,43 @@ namespace lab2
 
                 EnAr[ch] = En;
                 MAr[ch] = M;
-                CAr[ch] = C;
                 EerAr[ch] = Eer;
                 MerAr[ch] = Mer;
 
-                Console.WriteLine("T= " + t + " En= " + En + " M= " + M + " C= " + C + " Eer= " + Eer + " Mer= " + Mer);
-                
-                using (sw[ch] = new StreamWriter("s-" + sv[ch] + ".txt")) // безопасно открываем поток записываем в файл и его закрываем
+                Console.WriteLine("T= " + t + " En= " + En + " M= " + M + " Eer= " + Eer + " Mer= " + Mer);
+
+                if (ch == 0)
                 {
-                    for (int i = 0; i < N; i++)
+                    using (StreamWriter file1 = new StreamWriter("output.txt", false))
                     {
-                        for (int j = 0; j < N; j++)
-                        {
-                            sw[ch].WriteLine(i + "\t" + j + "\t" + 0 + "\t" + Math.Cos(a[i, j])
-                                + "\t" + Math.Sin(a[i, j]) + "\t" + 0);
-                        }
+                        
+                        file1.WriteLine(t + "\t" + En + "\t" + Eer + "\t" + M + "\t" + Mer);
+                        
                     }
                 }
+                else
+                {
+                    using (StreamWriter file1 = new StreamWriter("output.txt", true))
+                    {
+                        file1.WriteLine(t + "\t" + En + "\t" + Eer + "\t" + M + "\t" + Mer);
+                    }
+                }
+
                 ch++;
             }
 
             double T; // записываем в 3 файла для гнуплот (вынес из цикла Т ради скорости)
-            using (StreamWriter file1 = new StreamWriter(fn1))
-            {
-                T = 0.05;
-                for (int i = 0; i < 80; i++)
-                {
-                    T = Math.Round(T, 2);
-                    file1.WriteLine(T + " " + EnAr[i] + " " + EerAr[i]);
-                    T += 0.05;
-                }
-            }
-            using (StreamWriter file2 = new StreamWriter(fn2))
-            {
-                T = 0.05;
-                for (int i = 0; i < 80; i++)
-                {
-                    T = Math.Round(T, 2);
-                    file2.WriteLine(T + " " + MAr[i] + " " + MerAr[i]);
-                    T += 0.05;
-                }
-            }
-            using (StreamWriter file3 = new StreamWriter(fn3))
-            {
-                T = 0.05;
-                for (int i = 0; i < 80; i++)
-                {
-                    T = Math.Round(T, 2);
-                    file3.WriteLine(T + " " + CAr[i]);
-                    T += 0.05;
-                }
-            }
+            
+            
 
             Console.WriteLine("Всё:)");
 
             Console.ReadKey();
         }
 
-        public static double[] CreatSpinArray(int i, int j) // считаем соседей
+        public static int[] CreatSpinArray(int i, int j) // считаем соседей
         {
-            double[] s = new double[4];
+            int[] s = new int[4];
 
             //Вверх
             if (i > 0) s[0] = a[i - 1, j];
@@ -195,10 +187,10 @@ namespace lab2
         public static double CreatEnS(int ien, int jen) // считаем энергию спина
         {
             double en = 0;
-            double[] s = CreatSpinArray(ien, jen);
+            int[] s = CreatSpinArray(ien, jen);
             for (int i = 0; i < 4; i++)
             {
-                en += Math.Cos(a[ien, jen] - s[i]);
+                en += s[i] * a[ien, jen];
             }
 
             return en * (-1);
@@ -221,21 +213,19 @@ namespace lab2
 
         public static double Magnet() //Намагниченность системы
         {
-            double m, ms = 0, mc = 0;
+            double m = 0;
 
             for (int i = 0; i < N; i++)
             {
                 for (int j = 0; j < N; j++)
                 {
-                    ms += Math.Sin(a[i, j]);
-                    mc += Math.Cos(a[i, j]);
+                    m += a[i, j];
                 }
             }
 
-            m = ms * ms + mc * mc;
+            m /= (N * N);
 
-            m /= (N * N * N * N);
-
+            m = Math.Abs(m);
             return m;
         }
 
